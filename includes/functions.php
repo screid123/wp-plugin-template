fc6a5e7f376276e9e9e9ba0b5242a186a57aee41
Reid Linker <rlinker@redventures.com>
2021-10-21 11:19:35 -0400

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -10,16 +10,10 @@ namespace WP_Plugin_Template;
  * Display a notice about missing dependencies.
  */
 function display_missing_dependencies_notice(): void {
-	$message = sprintf(
-	/* translators: %s: documentation URL */
-		__( '{{NAME}} is missing required dependencies. <a href="%s" target="_blank" rel="noopener noreferer">Learn more.</a>', '{{TEXT_DOMAIN}}' ),
-		'{{URI}}#installation'
-	);
-
 	printf(
 		'<div class="notice notice-error"><p>%s</p></div>',
 		wp_kses(
-			$message,
+			__( '{{NAME}} is missing required dependencies. <a href="{{URI}}" target="_blank" rel="noopener noreferer">Learn more.</a>', '{{TEXT_DOMAIN}}' ),
 			[
 				'a' => [
 					'href'   => true,
